25a34e7cd99295ad4ee1c4e2bc39200dcce940c3
Day <daychongyang@gmail.com>
2019-11-20 20:01:30 +0800

    style: remove lib

--- packages/cli/lib/helper/highlights.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const chalk_1 = require("chalk");
-/** 强调 */
-exports.em = chalk_1.cyan.bold.italic;
-/** 信息 */
-exports.info = chalk_1.gray.underline;
-/** 错误 */
-exports.error = chalk_1.red;
