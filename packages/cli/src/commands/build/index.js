5f1e240da82ae5004d768392d9a343b5f23bed91
Day <daychongyang@gmail.com>
2019-12-02 17:01:12 +0800

    style: ts clean

--- packages/cli/src/commands/build/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var yargs_1 = require("yargs");
-var build_1 = require("@ez-fe/build");
-yargs_1.command({
-    command: 'build',
-    describe: 'Building for production',
-    handler: build_1.build,
-});
--- packages/cli/src/commands/dev/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var yargs_1 = require("yargs");
-var dev_1 = require("@ez-fe/dev");
-yargs_1.command({
-    command: 'dev',
-    describe: 'Start a dev server for development',
-    handler: dev_1.dev,
-});
