b620afffc973f740a7d3fa2fcd6ae66796258c6e
Day <daychongyang@gmail.com>
2019-12-16 15:25:17 +0800

    style: code

--- packages/core/src/get-webpack-config.ts
+++ packages/core/src/get-webpack-config.ts
@@ -6,13 +6,17 @@ import { EZ } from './interface';
 const debug = createDebug('ez:get-webpack-config');
 
 export async function getWebpackConfig(ez: EZ): Promise<Configuration> {
-	const { sourcePath } = ez;
 	let webpackConfig = null;
+	const { sourcePath } = ez;
 	const { getWebpackChainConfig } = await import('@ez-fe/config');
 	const { config, cwd } = ez;
 	const { chainConfig } = config;
 
-	const webpackChainConfig = getWebpackChainConfig({ cwd, sourcePath, ...(<Required<Config>>config) });
+	const webpackChainConfig = getWebpackChainConfig({
+		cwd,
+		sourcePath,
+		...(<Required<Config>>config),
+	});
 
 	if (chainConfig) {
 		webpackConfig = chainConfig(webpackChainConfig).toConfig();
