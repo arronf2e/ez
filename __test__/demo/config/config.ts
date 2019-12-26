5b01298584759a8bb9436434d39b481eb99aa75a
Day <daychongyang@gmail.com>
2019-12-26 21:08:21 +0800

    docs: webpack bundle

--- __test__/demo/config/config.ts
+++ __test__/demo/config/config.ts
@@ -1,7 +1,7 @@
 import { Config } from '@ez-fe/config';
 
 export default <Config>{
-	port: 9001,
+	port: 12138,
 	chainConfig(webpackChainConfig) {
 		webpackChainConfig.mode('development');
 		webpackChainConfig.optimization.minimize(false);
--- __test__/demo/src/index.tsx
+++ __test__/demo/src/index.tsx
@@ -1,8 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-ReactDOM.render(<>1</>, document.getElementById('root'));
-
-if (module['hot']) {
-	module['hot'].accept();
-}
+// import './index.css';
