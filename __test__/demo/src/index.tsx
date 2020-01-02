3db82ceb33bce0c10e831db3fc08b1884f684332
Day <daychongyang@gmail.com>
2020-01-02 12:04:46 +0800

    test: postcss

--- __test__/demo/src/index.tsx
+++ __test__/demo/src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.less';
 
 ReactDOM.render(<>{process.env.todo}</>, document.getElementById('root'));
